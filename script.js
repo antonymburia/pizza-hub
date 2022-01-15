1822673a3b666adc8b140c9bc2931b05247ec1bd
antonymburia <antonymburia10@gmail.com>
2022-01-16 01:57:22 +0300

    add to cart functionality  working

--- script.js
+++ script.js
@@ -5,4 +5,18 @@ function customerorder(amount, size, crust, toppings,totalprice) {
   this.toppings = toppings;
   this.totalprice = totalprice
 }
-$(document).ready
+
+$(document).ready(function(){
+  $("#getvalue").click(function() {
+    var inputamt = $("input#ammount").val();
+    var inputsize = parseInt($("#size").val());
+    var inputcrust= parseInt($("#crust").val());
+    var inputtoppings = parseInt($("#toppings").val());
+    var totalprice = inputamt * inputsize + inputcrust + inputtoppings; 
+
+    const neworder = new customerorder(inputamt, inputsize, inputcrust, inputtoppings, totalprice);
+    
+    $("#cart").append("<li>" + neworder.totalprice + "</li>");
+  });
+
+});
