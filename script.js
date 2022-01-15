4bd2988d9b58868525fe273b646ee51d66e725fc
antonymburia <antonymburia10@gmail.com>
2022-01-15 22:53:27 +0300

    create a construtor to create orders

---
+++ script.js
@@ -0,0 +1,7 @@
+function customerorder(amount, size, crust, toppings,totalprice) {
+  this.ammount = ammount;
+  this.size = size;
+  this.crust = crust;
+  this.toppings = toppings;
+  this.totalprice = totalprice
+}
