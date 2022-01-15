82074457e7293e11f94eeff0f8d531f044b542fa
antonymburia <antonymburia10@gmail.com>
2022-01-16 02:01:55 +0300

    input validation working as expected

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-function customerorder(amount, size, crust, toppings,totalprice) {
+//validate input
+function getamt() {
+  var beef = document.getElementById('ammount').value;
+  var chicken = document.getElementById('num').value;
+  var cheese = document.getElementById('qty').value;
+
+  if (beef > 0) { 
+    return beef;
+  } else {
+    if (chicken > 0) {
+      return chicken;
+    } else {
+      if (cheese > 0) {
+        return cheese;
+      } else {
+        alert("enter valid ammount");
+      }
+    }
+  }
+}
+function customerorder(ammount, size, crust, toppings,totalprice) {
   this.ammount = ammount;
   this.size = size;
   this.crust = crust;
@@ -8,7 +28,7 @@ function customerorder(amount, size, crust, toppings,totalprice) {
 
 $(document).ready(function(){
   $("#getvalue").click(function() {
-    var inputamt = $("input#ammount").val();
+    var inputamt = getamt();
     var inputsize = parseInt($("#size").val());
     var inputcrust= parseInt($("#crust").val());
     var inputtoppings = parseInt($("#toppings").val());
