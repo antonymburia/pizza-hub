f808fd903e0f9b96d0eae3cb831742150ef44a56
antonymburia <antonymburia10@gmail.com>
2022-01-16 04:06:34 +0300

    create an array to store pizza name and amount

--- script.js
+++ script.js
@@ -5,19 +5,22 @@ function getamt() {
   var cheese = document.getElementById('qty').value;
 
   if (beef > 0) { 
-    return beef;
+    return [beef, 'Beef Pizza'];
   } else {
     if (chicken > 0) {
-      return chicken;
+      return [chicken, 'Chicken Pizza'];
     } else {
       if (cheese > 0) {
-        return cheese;
+        return [cheese, 'Cheese Pizza'];
       } else {
         alert("enter valid ammount");
       }
     }
   }
 }
+
+
+
 function customerorder(ammount, size, crust, toppings,totalprice) {
   this.ammount = ammount;
   this.size = size;
@@ -28,17 +31,29 @@ function customerorder(ammount, size, crust, toppings,totalprice) {
 
 $(document).ready(function(){
   $("#getvalue").click(function() {
-    var inputamt = getamt();
+    var selectedpizza = getamt();
+    var inputamt = selectedpizza[0];
     var inputsize = parseInt($("#size").val());
     var inputcrust= parseInt($("#crust").val());
     var inputtoppings = parseInt($("#toppings").val());
+    var pizzasize = $( "#size option:selected" ).text();
+    var pizzacrust = $( "#crust option:selected" ).text();
+    var pizzatoppings = $( "#toppings option:selected" ).text();
+    var pizzaamt = $( "#toppings option:selected" ).text();
+    var pizzaname = selectedpizza[1]
     var totalprice = inputamt * inputsize + inputcrust + inputtoppings; 
-
+ 
     // clear form data after submission
+    if (inputamt > 0) {
+      document.getElementById("first").reset();
+      document.getElementById("2nd").reset();
+      document.getElementById("3rd").reset();
+    }
+
 
     const neworder = new customerorder(inputamt, inputsize, inputcrust, inputtoppings, totalprice);
     
-    $("#cart").append("<li>" + neworder.totalprice + "</li>");
+    $("#cart").append("<ul><li>" + neworder.totalprice + "</li></ul>" + pizzasize);
   });
 
 });
